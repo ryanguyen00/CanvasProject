4393f92a9761bc905918015da23e82c4281cb718
ryanguyen00 <ryan.c.nguyen@bellevuecollege.edu>
2022-03-17 15:08:10 -0700

    final draft

--- script.js
+++ script.js
@@ -65,6 +65,8 @@ let down = true;
 let counter = 0;
 
 
+
+
 //setting up images
 
 //background image
@@ -138,8 +140,8 @@ var update = function (modifier) {
 	if (40 in keysDown) { // Player holding down
 		hero.y += hero.speed * modifier;
 			
-		if(hero.y > (800-150)){
-				hero.y = 800-150;
+		if(hero.y > (800-120)){
+				hero.y = 800-120;
 			}
 
 		left = false;
@@ -181,49 +183,48 @@ var update = function (modifier) {
 		++monstersCaught;
 		reset();
 	}
+	if (counter == 70) {  // adjust this to change "walking speed" of 
+
+		curXFrame = ++curXFrame % frameCount; //Updating the sprite 
+	
+		// it will count 0,1,2,0,1,2,0, etc
+		counter = 0;
+		} else {
+		counter++;
+	}
+	
+	srcX = curXFrame * width;   //Calculating the x coordinate for 
+	
+	//if left is true,  pick Y dim of the correct row
+	if (left) {
+	//calculate srcY
+	srcY = trackLeft * height;
+	}
+	//if the right is true,   pick Y dim of the correct row
+	if (right) {
+	//calculating y coordinate for spritesheet
+	srcY = trackRight * height;
+	}
+	
+	if (up)  {
+		srcY = trackUp * height;
+	}
+	
+	if (down)  {
+		srcY = trackDown * height;
+	}
+	
+	
+	if (left == false && right == false && up == false && down == false) {
+	srcX = 1 * width;
+	srcY = 2 * height;
+	}
 };
 
 //curXFrame = ++curXFrame % frameCount; //Updating the sprite frame index
 // it will count 0,1,2,0,1,2,0, etc
 
-if (counter == 20) {  // adjust this to change "walking speed" of 
 
-	curXFrame = ++curXFrame % frameCount; //Updating the sprite 
-
-	// it will count 0,1,2,0,1,2,0, etc
-	counter = 0;
-	} else {
-	counter++;
-}
-
-
-
-srcX = curXFrame * width;   //Calculating the x coordinate for 
-
-//if left is true,  pick Y dim of the correct row
-if (left) {
-//calculate srcY
-srcY = trackLeft * height;
-}
-//if the right is true,   pick Y dim of the correct row
-if (right) {
-//calculating y coordinate for spritesheet
-srcY = trackRight * height;
-}
-
-if (up)  {
-	srcY = trackUp * height;
-}
-
-if (down)  {
-	srcY = trackDown * height;
-}
-
-
-if (left == false && right == false && up == false && down == false) {
-srcX = 1 * width;
-srcY = 2 * height;
-}
 
 // Draw everything
 var render = function () {
@@ -240,9 +241,7 @@ var render = function () {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 
-	
-
-	// Score
+	//Score
 	if (monstersCaught < 5) {
 		ctx.fillStyle = "rgb(250, 250, 250)";
 		ctx.font = "24px Helvetica";
@@ -261,11 +260,41 @@ var render = function () {
 		gradient.addColorStop(0, "rgb(255, 0, 128)");
 		gradient.addColorStop(1, "rgb(255, 153, 51)");
 		ctx.fillStyle = gradient;
-		ctx.fillText("Game Over!", 250, 300);
+		ctx.fillText("Game Over! You win", 250, 300);
 		soundWin.play();
 	}
+
+
+
 };
 
+//own running function independent of main() function.
+
+var countdownTime = 5;
+var Timer = setInterval(function(){
+  if(countdownTime < 0 && monstersCaught < 5){
+	clearInterval(Timer);
+	document.getElementById("countdown").innerHTML = "You lose!";
+
+  } 
+  else if (countdownTime >= 0 && monstersCaught >= 5) {
+	clearInterval(Timer);
+	document.getElementById("countdown").innerHTML = "You win!";
+
+  }
+  else {
+   document.getElementById("countdown").innerHTML = countdownTime + " seconds remaining";
+  }
+  countdownTime -= 1;
+  
+}, 1000);
+
+
+
+
+
+
+	
 var main = function () {
 	var now = Date.now();
 	var delta = now - then;
@@ -275,14 +304,21 @@ var main = function () {
 
 	then = now;
 
-	if (monstersCaught < 5) {
+	
+
+	if (monstersCaught < 5 && countdownTime >= 0) {
 		requestAnimationFrame(main);
 	}
+	
 	// Request to do this again ASAP
 
 };
 
+
+
+
 var then = Date.now();
 reset();
 main();
+
 
